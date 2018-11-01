ed13be7af21228b6ec4a5f4a09bfb522322dae94
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2018-11-01 17:31:48 -0500

    Removed unncessary src addresses

--- Sources/Flower/Message.swift
+++ Sources/Flower/Message.swift
@@ -88,12 +88,12 @@ public struct EndpointV6: MaybeDatable
 
 public enum Message
 {
-    case TCPOpenV4(EndpointV4, EndpointV4, StreamIdentifier)
-    case TCPOpenV6(EndpointV6, EndpointV6, StreamIdentifier)
+    case TCPOpenV4(EndpointV4, StreamIdentifier)
+    case TCPOpenV6(EndpointV6, StreamIdentifier)
     case TCPClose(StreamIdentifier)
     case TCPData(StreamIdentifier, Data)
-    case UDPDataV4(EndpointV4, EndpointV4, Data)
-    case UDPDataV6(EndpointV6, EndpointV6, Data)
+    case UDPDataV4(EndpointV4, Data)
+    case UDPDataV6(EndpointV6, Data)
 }
 
 extension Message: MaybeDatable
@@ -113,94 +113,74 @@ extension Message: MaybeDatable
         {
             case .TCPOpenV4Type:
                 let endpointSize = AddressSize.v4.rawValue + 2
-                guard let (srcData, rest) = tail.splitOn(position: UInt(endpointSize)) else
-                {
-                    return nil
-                }
-                
-                guard let (dstData, streamidData) = rest.splitOn(position: UInt(endpointSize)) else
+                guard let (dstData, streamidData) = tail.splitOn(position: UInt(endpointSize)) else
                 {
                     return nil
                 }
 
-                guard let src = EndpointV4(data: srcData), let dst = EndpointV4(data: dstData) else
+                guard let dst = EndpointV4(data: dstData) else
                 {
                     return nil
                 }
                 
                 let streamid = streamidData.uint64
                 
-                self = .TCPOpenV4(src, dst, streamid)
+                self = .TCPOpenV4(dst, streamid)
             case .TCPOpenV6Type:
                 let endpointSize = AddressSize.v6.rawValue + 2
-                guard let (srcData, rest) = tail.splitOn(position: UInt(endpointSize)) else
+                guard let (dstData, streamidData) = tail.splitOn(position: UInt(endpointSize)) else
                 {
                     return nil
                 }
                 
-                guard let (dstData, streamidData) = rest.splitOn(position: UInt(endpointSize)) else
-                {
-                    return nil
-                }
-                
-                guard let src = EndpointV6(data: srcData), let dst = EndpointV6(data: dstData) else
+                guard let dst = EndpointV6(data: dstData) else
                 {
                     return nil
                 }
                 
                 let streamid = streamidData.uint64
                 
-                self = .TCPOpenV6(src, dst, streamid)
-                case .TCPCloseType:
-                    let streamid = tail.uint64
+                self = .TCPOpenV6(dst, streamid)
+            case .TCPCloseType:
+                let streamid = tail.uint64
 
-                    self = .TCPClose(streamid)
-                case .TCPDataType:
-                    let endpointSize = AddressSize.v4.rawValue + 2
-                    guard let (streamidData, payload) = tail.splitOn(position: UInt(endpointSize)) else
-                    {
-                        return nil
-                    }
+                self = .TCPClose(streamid)
+            case .TCPDataType:
+                let endpointSize = AddressSize.v4.rawValue + 2
+                guard let (streamidData, payload) = tail.splitOn(position: UInt(endpointSize)) else
+                {
+                    return nil
+                }
 
-                    let streamid = streamidData.uint64
-                    
-                    self = .TCPData(streamid, payload)
+                let streamid = streamidData.uint64
+                
+                self = .TCPData(streamid, payload)
             case .UDPDataV4Type:
                 let endpointSize = AddressSize.v4.rawValue + 2
-                guard let (srcData, rest) = tail.splitOn(position: UInt(endpointSize)) else
+                guard let (dstData, payload) = tail.splitOn(position: UInt(endpointSize)) else
                 {
                     return nil
                 }
                 
-                guard let (dstData, payload) = rest.splitOn(position: UInt(endpointSize)) else
+                guard let dst = EndpointV4(data: dstData) else
                 {
                     return nil
                 }
                 
-                guard let src = EndpointV4(data: srcData), let dst = EndpointV4(data: dstData) else
-                {
-                    return nil
-                }
-                
-                self = .UDPDataV4(src, dst, payload)
+                self = .UDPDataV4(dst, payload)
             case .UDPDataV6Type:
                 let endpointSize = AddressSize.v6.rawValue + 2
-                guard let (srcData, rest) = tail.splitOn(position: UInt(endpointSize)) else
+                guard let (dstData, payload) = tail.splitOn(position: UInt(endpointSize)) else
                 {
                     return nil
                 }
                 
-                guard let (dstData, payload) = rest.splitOn(position: UInt(endpointSize)) else
+                guard let dst = EndpointV6(data: dstData) else
                 {
                     return nil
                 }
                 
-                guard let src = EndpointV6(data: srcData), let dst = EndpointV6(data: dstData) else
-                {
-                    return nil
-                }
-                
-                self = .UDPDataV6(src, dst, payload)
+                self = .UDPDataV6(dst, payload)
         }
     }
     
@@ -208,14 +188,12 @@ extension Message: MaybeDatable
         var result = Data()
         switch self
         {
-            case .TCPOpenV4(let src, let dst, let streamid):
+            case .TCPOpenV4(let dst, let streamid):
                 result.append(MessageType.TCPOpenV4Type.rawValue)
-                result.append(src.data)
                 result.append(dst.data)
                 result.append(streamid.data)
-            case .TCPOpenV6(let src, let dst, let streamid):
+            case .TCPOpenV6(let dst, let streamid):
                 result.append(MessageType.TCPOpenV6Type.rawValue)
-                result.append(src.data)
                 result.append(dst.data)
                 result.append(streamid.data)
             case .TCPClose(let streamid):
@@ -225,14 +203,12 @@ extension Message: MaybeDatable
                 result.append(MessageType.TCPDataType.rawValue)
                 result.append(streamid.data)
                 result.append(payload)
-            case .UDPDataV4(let src, let dst, let payload):
+            case .UDPDataV4(let dst, let payload):
                 result.append(MessageType.UDPDataV4Type.rawValue)
-                result.append(src.data)
                 result.append(dst.data)
                 result.append(payload)
-            case .UDPDataV6(let src, let dst, let payload):
+            case .UDPDataV6(let dst, let payload):
                 result.append(MessageType.UDPDataV6Type.rawValue)
-                result.append(src.data)
                 result.append(dst.data)
                 result.append(payload)
         }
