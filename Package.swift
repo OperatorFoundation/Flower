bc2f99b40ee88a9673c21170c97b4c1882ef475a
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2021-11-05 02:36:07 -0500

    Removed obsolete Transport dependeny

--- Package.swift
+++ Package.swift
@@ -17,17 +17,16 @@ name: "Flower",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/OperatorFoundation/Datable", from: "3.1.0"),
-        .package(url: "https://github.com/OperatorFoundation/Transport", from: "2.3.7"),
         .package(url: "https://github.com/apple/swift-log", from: "1.4.2"),
         .package(url: "https://github.com/OperatorFoundation/SwiftQueue", from: "0.1.1"),
-        .package(url: "https://github.com/OperatorFoundation/TransmissionTransport", from: "0.2.10")
+        .package(url: "https://github.com/OperatorFoundation/Transmission", from: "0.6.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Flower",
-            dependencies: ["Datable", "Transport", "SwiftQueue", "TransmissionTransport", .product(name: "Logging", package: "swift-log")]),
+            dependencies: ["Datable", "SwiftQueue", "Transmission", .product(name: "Logging", package: "swift-log")]),
         .testTarget(
             name: "FlowerTests",
             dependencies: ["Flower"]),
