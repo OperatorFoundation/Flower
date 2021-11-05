795bb5bf7f24a630cda502e0ad1ac03c0a0caf97
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2021-11-05 16:46:14 -0500

    Updated Transmission

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ name: "Flower",
         .package(url: "https://github.com/OperatorFoundation/Datable", from: "3.1.0"),
         .package(url: "https://github.com/apple/swift-log", from: "1.4.2"),
         .package(url: "https://github.com/OperatorFoundation/SwiftQueue", from: "0.1.1"),
-        .package(url: "https://github.com/OperatorFoundation/Transmission", from: "0.6.0")
+        .package(url: "https://github.com/OperatorFoundation/Transmission", from: "1.0.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
