47d0e3447a50ecbaa8bd185cd6a57d74797a7643
CryptoSax <joshua@operatorfoundation.org>
2021-11-29 14:24:18 -0600

    added debug prints

--- Sources/Flower/MessageStream.swift
+++ Sources/Flower/MessageStream.swift
@@ -16,26 +16,41 @@ extension Transmission.Connection
     {
         guard let data = self.read(size: 2) else
         {
+            if let logger = log {
+                logger.error("flower failed to read message length")
+            }
             return nil
         }
 
         guard let uint16Length = UInt16(maybeNetworkData: data) else {
+            if let logger = log {
+                logger.error("flower failed to conver message length to UInt16")
+            }
             return nil
         }
 
         let length = Int(uint16Length)
 
         if length > 1600 {
+            if let logger = log {
+                logger.error("flower read size too big")
+            }
             return nil
         }
 
         guard let data = self.read(size: length) else
         {
+            if let logger = log {
+                logger.error("flower failed to read message")
+            }
             return nil
         }
 
         guard let message = Message(data: data) else
         {
+            if let logger = log {
+                logger.error("flower failed to parse data as message")
+            }
             return nil
         }
 
