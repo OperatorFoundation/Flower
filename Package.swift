81dbcde19c80ce93db9e61b1cc270ae753e78d3c
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2022-09-27 15:16:56 -0500

    Minor formatting change

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
 name: "Flower",
     platforms: [
-       .macOS(.v12), .iOS(.v15)
+       .macOS(.v12),
+       .iOS(.v15),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
