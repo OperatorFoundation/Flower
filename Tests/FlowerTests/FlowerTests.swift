7c40a5b9a852ab4564302c442ab3a771763e2d90
consuelita <adelita@operatorfoundation.org>
2022-11-18 14:32:31 -0600

    Update FlowerTests.swift

--- Tests/FlowerTests/FlowerTests.swift
+++ Tests/FlowerTests/FlowerTests.swift
@@ -1,10 +1,19 @@
-import Datable
+
 @testable import Flower
-import InternetProtocols
+
+#if os(macOS) || os(iOS)
+import os.log
+#else
 import Logging
+#endif
+
+import XCTest
+
+import Datable
+import InternetProtocols
 import Net
 import Transmission
-import XCTest
+
 
 final class FlowerTests: XCTestCase
 {
@@ -95,7 +104,7 @@ final class FlowerTests: XCTestCase
             return
         }
         
-        guard let transmissionConnection: Transmission.Connection = TransmissionConnection(host: "159.203.108.187", port: 1234) else
+        guard let transmissionConnection: Transmission.Connection = TransmissionConnection(host: "", port: 1234) else
         {
             XCTFail()
             return
@@ -203,7 +212,7 @@ final class FlowerTests: XCTestCase
             return
         }
 
-        guard let transmissionConnection: Transmission.Connection = TransmissionConnection(host: "206.189.200.18", port: 1234) else
+        guard let transmissionConnection: Transmission.Connection = TransmissionConnection(host: "164.92.71.230", port: 1234) else
         {
             XCTFail()
             return
@@ -240,7 +249,12 @@ final class FlowerTests: XCTestCase
 
     func testClientServer()
     {
-        let logger = Logger(label: "FlowerTests")
+        #if os(macOS) || os(iOS)
+        let logger = Logger(subsystem: "org.OperatorFoundation.Flower", category: "FlowerTests")
+        #else
+        let logger = Logger(label: "org.OperatorFoundation.Flower")
+        #endif
+        
         let queue = DispatchQueue(label: "FlowerTests.testClientServer.server")
         let lock = DispatchGroup()
         let serverRead = expectation(description: "server read")
