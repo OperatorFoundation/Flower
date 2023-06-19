21d270b489c7d66499433ff7a15a2cd6b53c23b0
CryptoSax <joshua@operatorfoundation.org>
2023-06-19 14:40:20 -0500

    updated to use swift tools 5.8 and macos13

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
 name: "Flower",
     platforms: [
-       .macOS(.v12),
+       .macOS(.v13),
        .iOS(.v15),
     ],
     products: [
